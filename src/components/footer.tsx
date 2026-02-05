08635fb15919b30f4bd52eca0777ef3c1cdd4d82
Ayush Bhagel <singhbaghelayush99@gmail.com>
2026-02-05 18:04:09 +0530

    Chnages in Design

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="flex items-center justify-center gap-2">
                 <span className="text-black dark:text-gray-400">Made by</span>
                 <Link href="https://www.linkedin.com/in/ayush-singh-baghel-1a217a30a/" target="_blank">
-                    <div className="text-sky-600 dark:text-sky-400 inline-flex items-center gap-1 hover:underline"> <FaLinkedin className="w-4 h-4" /> Sahil Sawal </div>
+                    <div className="text-sky-600 dark:text-sky-400 inline-flex items-center gap-1 hover:underline"> <FaLinkedin className="w-4 h-4" /> Ayush Singh Baghel </div>
                 </Link>
                 <span className="text-black dark:text-gray-400">in 2026</span>
                 <span className="text-black dark:text-gray-400">•</span>
